28718b42d42ec1439089f8b78f58b65505c5990e
Riyad <pycoder028@gmail.com>
2024-09-18 16:12:37 +0600

    Admin Dashboard integration & Setup project file structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include('includes/header.php'); ?>
+
+    <div class="py-5">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 style="color: blue;">POS System in PHP MYSQL</h1>
+
+                    <a href="login.php" class="btn btn-success mt-4">Login</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include('includes/footer.php'); ?>
